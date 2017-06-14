34ef69e9ecf72ea8fa75baf1333485e0e77c6d58
Isis Pamela Rios <isis.rios@fpl.com>
2017-06-14 11:12:14 -0400

    packages

---
+++ WeatherApp/WeatherApp.Droid/MainActivity.cs
@@ -0,0 +1,87 @@
+﻿
+
+
+using System;
+using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Graphics;
+using System.Net;
+
+namespace WeatherApp.Droid
+{
+    [Activity(Label = " Weather App", MainLauncher = true, Icon = "@drawable/sun")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.Main);
+
+            Button button = FindViewById<Button>(Resource.Id.weatherBtn);
+
+            button.Click += Button_Click;
+        }
+
+		private Bitmap GetImageBitmapFromUrl(string url)
+		{
+			Bitmap imageBitmap = null;
+
+			using (var webClient = new WebClient())
+			{
+				var imageBytes = webClient.DownloadData(url);
+				if (imageBytes != null && imageBytes.Length > 0)
+				{
+					imageBitmap = BitmapFactory.DecodeByteArray(imageBytes, 0, imageBytes.Length);
+				}
+			}
+
+			return imageBitmap;
+		}
+
+        private async void Button_Click(object sender, EventArgs e)
+        {
+            EditText zipCodeEntry = FindViewById<EditText>(Resource.Id.zipCodeEntry);
+
+            /*
+			AlertDialog.Builder alert = new AlertDialog.Builder(this);
+			alert.SetTitle("Debugging");
+			alert.SetMessage("BREAKPOINT 1");
+            Dialog dialog = alert.Create();
+            dialog.Show();
+            */
+
+            if (!String.IsNullOrEmpty(zipCodeEntry.Text))
+            {
+                Weather weather = await Core.GetWeather(zipCodeEntry.Text);
+                if (weather != null)
+				{
+                    FindViewById<TextView>(Resource.Id.locationText).Text = weather.Title;
+                    FindViewById<TextView>(Resource.Id.tempText).Text = weather.Temperature;
+                    FindViewById<TextView>(Resource.Id.windText).Text = weather.Wind;
+                    FindViewById<TextView>(Resource.Id.visibilityText).Text = weather.Visibility;
+                    FindViewById<TextView>(Resource.Id.humidityText).Text = weather.Humidity;
+					FindViewById<TextView>(Resource.Id.sunriseText).Text = weather.Sunrise;
+					FindViewById<TextView>(Resource.Id.sunsetText).Text = weather.Sunset;
+
+                    var bitmap = GetImageBitmapFromUrl("http://openweathermap.org/img/w/" + weather.Icon + ".png");
+					FindViewById<ImageView>(Resource.Id.imgWeather).SetImageBitmap(bitmap);
+
+                }
+            }
+
+
+			// We retrieve the image
+			
+
+				//IWeatherImageProvider provider = new WeatherImageProvider();
+			 //   provider.getImage(code, requestQueue, new IWeatherImageProvider.WeatherImageListener() {
+    //            @Override
+    //            public void onImageReady(Bitmap image) {
+				//weatherImage.setImageBitmap(image);
+				//}
+		
+        }
+    }
+}
---
+++ WeatherApp/WeatherApp/Core.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace WeatherApp
+{
+    public class Core
+    {
+        public static async Task<Weather> GetWeather(string zipCode)
+        {
+            //Sign up for a free API key at http://openweathermap.org/appid
+            string key = "c2c1a0fd0187f7e168bce4c6d6c4a723";
+            string queryString = "http://api.openweathermap.org/data/2.5/weather?zip="
+                + zipCode + "&appid=" + key;
+
+            var results = await DataService.getDataFromService(queryString).ConfigureAwait(false);
+
+            if (results["weather"] != null)
+            {
+                Weather weather = new Weather();
+                weather.Title = (string)results["name"];
+                weather.Temperature = (string)results["main"]["temp"]  + " F";
+                weather.Wind = (string)results["wind"]["speed"] + " mph";
+                weather.Humidity = (string)results["main"]["humidity"] + " %";
+                weather.Visibility = (string)results["weather"][0]["main"];
+				weather.Icon = (string)results["weather"][0]["icon"];
+               
+
+                DateTime time = new System.DateTime(1970, 1, 1, 0, 0, 0, 0);
+                DateTime sunrise = time.AddSeconds((double)results["sys"]["sunrise"]);
+                DateTime sunset = time.AddSeconds((double)results["sys"]["sunset"]);
+                weather.Sunrise = sunrise.ToString() + " UTC";
+                weather.Sunset = sunset.ToString() + " UTC";
+                return weather;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
